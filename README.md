fe29a801f5eee73faff85e6a5b6f01fcdd76b535
Paul Tobin <tobster111@gmail.com>
2020-04-21 18:43:07 +0100

    Match anything with a wildcard period

--- README.md
+++ README.md
@@ -30,6 +30,19 @@ let codingRegex = /coding/;
 let result5 = extractStr.match(codingRegex); 
 ```
 
+```js
+// Match Anything with Wildcard Period
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+humStr.match(huRegex); // Returns ["hum"]
+hugStr.match(huRegex); // Returns ["hug"]
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let result = unRegex.test(exampleStr);
+```
+
 ****
 ## Flags i, g, m, s, u, y
 `Flag i - With this flag the search is case-insensitive`
--- index.js
+++ index.js
@@ -7,33 +7,46 @@ let myRegex = /Hello/;
 let result1 = myRegex.test(myString); //?
 
 // Match literal strings (Case sensitive)
-let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoIsHiding = 'Somewhere Waldo is hiding in this text.';
 let waldoRegex = /Waldo/;
 let result2 = waldoRegex.test(waldoIsHiding); //?
 
 // Or operator single pipe | allows for mutiple different possibilities
-let petString = "James has a pet cat.";
+let petString = 'James has a pet cat.';
 let petRegex = /dog|cat|bird|fish/;
 let result3 = petRegex.test(petString); //?
 
 // Extract Match Method
 let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/; 
-let result5 = extractStr.match(codingRegex); 
+let codingRegex = /coding/;
+let result5 = extractStr.match(codingRegex);
 console.log(result5);
 
 // Flag i - search is case-insensitive
-let myStringi = "freeCodeCamp";
-let fccRegex = /freecodecamp/i; 
+let myStringi = 'freeCodeCamp';
+let fccRegex = /freecodecamp/i;
 let result4 = fccRegex.test(myStringi); //?
 
 // Flag g - search looks for all matches
-let testStr = "Repeat, Repeat, Repeat";
+let testStr = 'Repeat, Repeat, Repeat';
 let ourRegex = /Repeat/g;
 testStr.match(ourRegex);
 
-let twinkleStar = "Twinkle, twinkle, little star";
-let starRegex = /twinkle/ig;
-let result6 = twinkleStar.match(starRegex); 
+let twinkleStar = 'Twinkle, twinkle, little star';
+let starRegex = /twinkle/gi;
+let result6 = twinkleStar.match(starRegex);
 
 console.log(result6);
+
+// Match Anything with Wildcard Period
+let humStr = "I'll hum a song";
+let hugStr = 'Bear hug';
+let huRegex = /hu./;
+humStr.match(huRegex); // Returns ["hum"]
+hugStr.match(huRegex); // Returns ["hug"]
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let result = unRegex.test(exampleStr);
+
+console.log(result);
